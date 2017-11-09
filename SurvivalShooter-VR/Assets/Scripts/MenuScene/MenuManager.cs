3f989d85015131b13f002e64d58989b2924fde16
habq <quangha150195@gmail.com>
2017-11-09 18:42:35 +0700

    Animation button menu

--- SurvivalShooter-VR/Assets/Scripts/MenuScene/MenuManager.cs
+++ SurvivalShooter-VR/Assets/Scripts/MenuScene/MenuManager.cs
@@ -11,32 +11,53 @@ public class MenuManager : MonoBehaviour {
   [SerializeField]
   private GameObject m_BtnGroup;
   [SerializeField]
-  private Button m_btnPlay;
+  private GameObject m_BtnAbout;
   [SerializeField]
   private GameObject m_BtnSelectLevel;
+
   RaycastHit hit;
+  private int m_Once;
 	// Use this for initialization
 	void Start () {
-	
+	  
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	  Physics.Raycast(m_Recticle.transform.position, m_Recticle.transform.forward, out hit); 
-    if(hit.collider.tag == "VRMenu" && GvrViewer.Instance.Triggered)
+    GameObject a = new GameObject();
+    if(hit.collider.tag == "VRMenu")
     {
-          m_Player.GetComponent<PlayerController>().m_move = true;
-          Invoke("SetActive", 1);
-          Debug.Log("okokok");
+          GameObject _thisButton = hit.collider.gameObject;
+          iTween.ScaleTo(_thisButton, iTween.Hash("x", 1.5f, "y", 1.5f, "time", 0.3f));
+          if(GvrViewer.Instance.Triggered)
+          {
+              m_Player.GetComponent<PlayerController>().m_move = true;
+              _thisButton.GetComponent<Button>().onClick.Invoke();
+          }
+          _thisButton = a;
+    }
+    else
+    {
+      iTween.ScaleTo(a, iTween.Hash("x", 1.0f, "y", 1.0f, "time", 0.3f));
     }
 	}
 
 
-  public void SetActive()
+  public void btnPlay()
   {
+      
       m_BtnGroup.SetActive(false);
       m_BtnSelectLevel.SetActive(true);
   }
+
+  public void btnAbout()
+  {
+      m_BtnGroup.SetActive(false);
+      m_BtnAbout.SetActive(true);
+  }
+
+
 
 
 
